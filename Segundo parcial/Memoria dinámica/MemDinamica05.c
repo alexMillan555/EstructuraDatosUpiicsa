afc3d3d420c6b6135b7a258003d57ed8efea3ccc
Alejandro Millan Jimenez <alexmillanjimenez@hotmail.com>
2025-11-12 13:25:31 -0600

    SUBIDA CAMBIOS ALEX

---
+++ Segundo parcial/Memoria dinámica/MemDinamica05.c
@@ -0,0 +1,36 @@
+/*MemDinamica05.c
+	Funciones de asignacion para memoria dinamica
+	Asigna suficiente memoria para almacenar estructuras tipo dir
+	Transcibrió: Alejandro Millán Jiménez
+	Compiló: Alejandro Millán Jiménez
+	Fecha de compilación: 05/11/2025 09:59
+	
+	*/	
+	
+#include<stdlib.h>
+#include<stdio.h>
+struct dir
+{
+	char nombre[20];
+	char calle[40];
+	char ciudad[40];
+	char CP[5];
+};
+
+int main()
+{
+	struct dir *p;
+	p = malloc(sizeof(struct dir));
+	
+	if(p==NULL)
+	{
+		printf("Error en la asignacion\n");
+		exit(1);
+	}
+	
+	printf("Direccion de memoria donde apunta p: %p en dec: %d \n",p,p);
+	p++;
+	printf("Si se incrementara en 1 a p: %p en dec: %d\n",p,p);
+	free(p);
+	return 0;
+}
---
+++ Segundo parcial/Memoria dinámica/apuntador-estructura.cpp
@@ -0,0 +1,12 @@
+/*apuntador-estructura.cpp
+	Apuntadores a estructuras
+	Transcibrió: Alejandro Millán Jiménez
+	Compiló: Alejandro Millán Jiménez
+	Fecha de compilación: 05/11/2025 10:24
+	
+	*/	
+	
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
