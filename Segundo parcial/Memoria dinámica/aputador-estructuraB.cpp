bc0d0d370d322100a433c157480b4f0397f71fd8
Alejandro Millan Jimenez <alexmillanjimenez@hotmail.com>
2025-11-12 14:22:05 -0600

    TERMINO ARCHIVO 1 ALEX

---
+++ Segundo parcial/Memoria dinámica/aputador-estructuraB.cpp
@@ -0,0 +1,45 @@
+/*apuntador-estructuraB.cpp
+	Apuntadores a estructuras
+	Transcibrió: Alejandro Millán Jiménez
+	Compiló: Alejandro Millán Jiménez
+	Fecha de compilación: 12/11/2025 14:02
+	
+	*/	
+	
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h> //para new y delete
+using namespace std;
+
+struct Persona
+{
+	char nombre[15];
+	int edad;
+}*apuntador_persona = new(Persona); //Se utiliza la funcion new para solicitar espacio del heap
+
+void pedirDatos();
+void mostrarDatos(Persona *);
+
+int main()
+{
+	pedirDatos();
+	mostrarDatos(apuntador_persona);
+	cout<<"Direccion de la variable: "<<apuntador_persona<<"\n"<<endl;
+	getch();
+	return 0;
+}
+
+void pedirDatos()
+{
+	cout<<"Digite el nombre: ";
+	//sin apuntadores el acceso a los miembros seria: (persona.nombre)
+	cin.getline(apuntador_persona->nombre,15,'\n');
+	cout<<"Digita la edad: ";
+	cin>>apuntador_persona->edad;
+}
+
+void mostrarDatos(Persona *apuntador_persona)
+{
+	cout<<"\nNombre almacenado: "<<&apuntador_persona->nombre<<endl;
+	cout<<"\nEdad almacenada: "<<&apuntador_persona->edad<<endl;
+}
